c1df357d792c384abf96d0040cf49319a0be0334
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-03 14:32:59 -0300

    first commit

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@interfaces": "./src/interfaces",
+            "@hooks": "./src/hooks",
+            "@pages": "./src/pages",
+            "@routes": "./src/routes",
+            "@services": "./src/services",
+            "@styles": "./src/styles"
+          },
+          extensions: ['.js', '.tsx', '.jsx', '.ios.js', '.android.js'],
+        },
+      ], 
+      ['styled-components'],
+    ],
   };
 };
