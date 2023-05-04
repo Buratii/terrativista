05e1e792f99599e5717e6b0d4034a3676159dc8c
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-04 17:43:47 -0300

    feat: create theme configuration

---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+export default {
+  colors: {
+    white: '#FFFFFF',
+
+    green_700: '#276749',
+    green_600: '#2F855A',
+    green_500: '#38A169',
+    green_400: '#48BB78',
+
+    red_700: '#9B2C2C',
+    red_600: '#C53030',
+    red_500: '#E53E3E',
+    red_400: '#F56565',
+
+    gray_700: '#2D3748',
+    gray_600: '#4A5568',
+    gray_500: '#718096',
+    gray_400: '#A0AEC0',
+    gray_300: '#CBD5E0',
+    gray_200: '#E2E8F0',
+    gray_100: '#EDF2F7',
+    gray_50: '#F7FAFC',
+
+    black_900: '#141414',
+    black_800: '#333333',
+    black_700: '#5c5c5c',
+    black_600: '#858585',
+    black_500: '#a3a3a3',
+    black_400: '#c2c2c2',
+  },
+  fonts: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+  },
+  sizes: {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+  },
+}
